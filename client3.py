169cbff7b505be7d026a94308260136425d8adf2
Michelkazi <michel.m.kazi@gmail.com>
2020-01-16 17:25:00 -0500

    updated get_data_point and get_ratio to return the correct data

--- client3.py
+++ client3.py
@@ -32,17 +32,21 @@ N = 500
 def getDataPoint(quote):
 	""" Produce all of the needed values to generate a datapoint """
 	""" ------------- Update this function ------------- """
+
 	stock = quote['stock']
 	bid_price = float(quote['top_bid']['price'])
 	ask_price = float(quote['top_ask']['price'])
-	price = bid_price
+	price = ( bid_price + ask_price ) / 2
 	return stock, bid_price, ask_price, price
 
 def getRatio(price_a, price_b):
 	""" Get ratio of price_a and price_b """
 	""" ------------- Update this function ------------- """
 	""" Also create some unit tests for this function in client_test.py """
-	return 1
+        try: 
+            return price_a/price_b 
+        except: 
+            print(f'{b} is invalid')
 
 # Main
 if __name__ == "__main__":
